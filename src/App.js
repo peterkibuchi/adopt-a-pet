47802695cf31267208f359c7bb4986bb56598535
Peter Kibuchi <68213317+peterkibuchi@users.noreply.github.com>
2022-03-02 08:06:43 +0300

    Complete Project

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import HomePage from './pages/home';
 import SearchPage from './pages/search';
 import PetDetailsPage from './pages/detail';
@@ -6,10 +7,25 @@ import Navigation from './components/navigation';
 
 function App() {
   return (
-    <div>
-      <Navigation />
-      <HomePage />
-    </div>
+    <Router>
+      <div>
+        <Navigation />
+        <Switch>
+          <Route path={`/search`}>
+            <SearchPage />
+          </Route>
+          <Route path={`/pet-details-not-found`}>
+            <PetDetailsNotFound />
+          </Route>
+          <Route path={`/:type/:id`}>
+            <PetDetailsPage />
+          </Route>
+          <Route path={`/:type?`}>
+            <HomePage />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
--- src/pages/detail/index.js
+++ src/pages/detail/index.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import { Redirect, useParams } from 'react-router-dom';
 import { getPetDetails } from '../../api/petfinder';
 import Hero from '../../components/hero';
 
@@ -6,7 +7,7 @@ const PetDetailsPage = () => {
   const [data, setData] = useState();
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
-  const id = '51322435'; // <--- Update me!
+  const { id } = useParams(); // <--- Update me!
 
   useEffect(() => {
     async function getPetsData() {
@@ -30,6 +31,7 @@ const PetDetailsPage = () => {
       ) : error ? (
         <div>
           {/* Redirect to /pet-details-not-found if there was an error! */}
+          <Redirect to={`/pet-details-not-found`} />
         </div>
       ) : (
         <main>
